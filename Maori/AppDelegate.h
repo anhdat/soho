90e9629ae7093d6dd70c58e9f147381642d4d74d
anhdat <mr.anhdat@gmail.com>
2013-02-27 21:36:13 +0700

    Initial Commit

---
+++ Maori/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Maori
+//
+//  Created by Dat Anh Truong on 2/27/13.
+//  Copyright (c) 2013 Dat Anh Truong. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Maori/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Maori
+//
+//  Created by Dat Anh Truong on 2/27/13.
+//  Copyright (c) 2013 Dat Anh Truong. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
